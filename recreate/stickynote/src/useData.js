3b0c5a03729bba584e89b4822c0ddbc4eee9d4da
dreywesson <dreywesson@gmail.com>
2022-09-05 09:35:47 +0100

    sticky note app

--- recreate/stickynote/src/useData.js
+++ recreate/stickynote/src/useData.js
@@ -1,27 +1,16 @@
 import { useEffect, useState } from "react";
 import { actionTypes } from "./reducer";
 import { useStateValue } from "./StateProvider";
-
 export const useData = () => {
   const [data, setData] = useState([]);
   const [{ notes }, dispatch] = useStateValue();
+  const { ADD_NOTE, DELETE_NOTE, SET_NOTES } = actionTypes;
 
-  const handleSubmit = (formData) => {
-    // console.log(formData);
-    dispatch({ type: actionTypes.ADD_NOTE, note: formData });
-  };
-
-  const handleDelete = (id) => {
-    dispatch({ type: actionTypes.DELETE_NOTE, id });
-  };
-
-  useEffect(() => {
-    dispatch({ type: actionTypes.SET_NOTES });
-  }, [dispatch]);
-
-  useEffect(() => {
-    setData(notes);
-  }, [notes]);
+  const handleSubmit = (formData) =>
+    dispatch({ type: ADD_NOTE, note: formData });
+  const handleDelete = (id) => dispatch({ type: DELETE_NOTE, id });
+  useEffect(() => dispatch({ type: SET_NOTES }), [dispatch, SET_NOTES]);
+  useEffect(() => setData(notes), [notes]);
 
   return { data, setData, handleSubmit, handleDelete };
 };
