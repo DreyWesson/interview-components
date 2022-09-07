c38391e17b88a46d91d11720a00de88fd1e41b79
dreywesson <dreywesson@gmail.com>
2022-09-07 09:45:18 +0100

    wordle app: useData Hook setup

--- recreate/wordle/src/App.js
+++ recreate/wordle/src/App.js
@@ -1,59 +1,12 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import { Guess } from "./components/Guess";
 import { Qwerty } from "./components/Qwerty";
 import { useStateValue } from "./StateProvider";
-import { actionTypes } from "./reducer";
+import { useData } from "./useData";
 
 const App = () => {
-  const [state, dispatch] = useStateValue();
-  const [data, setData] = useState(state);
-
-  // console.log(data);
-  // const memoState = useRef(state);
-
-  useEffect(() => {
-    const handleSubmit = () => {
-      // if (state?.words.includes(state.guesses[state.currentGuess])) {
-      dispatch({ type: actionTypes.SET_WINNER });
-      dispatch({ type: actionTypes.INCREMENT });
-      dispatch({ type: actionTypes.SET_LOSER });
-      dispatch({ type: actionTypes.ALL_GUESSES });
-      // }
-    };
-    const handleKeyUp = (e) => {
-      dispatch({ type: actionTypes.SET_WINNER });
-      dispatch({ type: actionTypes.SET_LOSER });
-      if (state?.loser || state?.winner) return;
-      if (e.key === "Enter") return handleSubmit();
-      if (e.key === "Backspace") {
-        dispatch({ type: actionTypes.BACKSPACE });
-        return;
-      }
-      if (
-        state.guesses[state.currentGuess].length < 5 &&
-        e.key.match(/^[A-z]$/)
-      ) {
-        state.guesses[state.currentGuess] += e.key.toLowerCase();
-        setData({ ...state });
-        // console.log(memoState.current);
-        // dispatch new state changes here
-      }
-    };
-    dispatch({ type: actionTypes.SET_GAME, action: "set" });
-    window.addEventListener("keyup", handleKeyUp);
-    return () => window.removeEventListener("keyup", handleKeyUp);
-  }, [
-    dispatch,
-    // memoState,
-    state?.word,
-    state?.guesses,
-    state?.isGuessed,
-    state.currentGuess,
-    state?.loser,
-    state?.winner,
-    state?.words,
-  ]);
-  // console.log(data);
+  const [{ guesses, word, currentGuess, winner, loser }] = useStateValue();
+  const { handleReset } = useData();
 
   return (
     <div
@@ -68,21 +21,21 @@ const App = () => {
     >
       <div className="" style={{ textAlign: "center" }}>
         <h1>Wordle</h1>
-        {state?.guesses.map((el, i) => {
+        {guesses.map((el, i) => {
           return (
             <Guess
-              word={data?.word}
-              guess={data?.guesses[i]}
-              isGuessed={i < data?.currentGuess}
+              word={word}
+              guess={guesses[i]}
+              isGuessed={i < currentGuess}
               current={i}
               key={i}
             />
           );
         })}
 
-        {state.winner && <h1>You won!!</h1>}
-        {state.loser && <h1>You lost!!</h1>}
-        {(state.winner || state.loser) && (
+        {winner && <h1>You won!!</h1>}
+        {loser && <h1>You lost!!</h1>}
+        {(winner || loser) && (
           <button
             style={{
               padding: "15px",
@@ -92,9 +45,7 @@ const App = () => {
               marginBottom: "20px",
               boxShadow: "rgb(0 0 0 / 75%) 2px 3px 8px 0px",
             }}
-            onClick={() =>
-              dispatch({ type: actionTypes.SET_GAME, action: "reset" })
-            }
+            onClick={() => handleReset()}
           >
             Play Again
           </button>
--- recreate/wordle/src/useData.js
+++ recreate/wordle/src/useData.js
@@ -3,24 +3,56 @@ import { actionTypes } from "./reducer";
 import { useStateValue } from "./StateProvider";
 
 export const useData = () => {
-  const [data, setData] = useState([]);
-  const [{ notes }, dispatch] = useStateValue();
+  const [state, dispatch] = useStateValue();
+  const [data, setData] = useState(state);
 
-  const handleSubmit = (formData) => {
-    dispatch({ type: actionTypes.ADD_NOTE, note: formData });
-  };
-
-  const handleDelete = (id) => {
-    dispatch({ type: actionTypes.DELETE_NOTE, id });
-  };
+  // console.log(data);
+  // const memoState = useRef(state);
 
   useEffect(() => {
-    dispatch({ type: actionTypes.SET_NOTES });
-  }, [dispatch]);
+    const handleSubmit = () => {
+      // if (state?.words.includes(state.guesses[state.currentGuess])) {
+      dispatch({ type: actionTypes.SET_WINNER });
+      dispatch({ type: actionTypes.INCREMENT });
+      dispatch({ type: actionTypes.SET_LOSER });
+      dispatch({ type: actionTypes.ALL_GUESSES });
+      // }
+    };
+    const handleKeyUp = (e) => {
+      dispatch({ type: actionTypes.SET_WINNER });
+      dispatch({ type: actionTypes.SET_LOSER });
+      if (state?.loser || state?.winner) return;
+      if (e.key === "Enter") return handleSubmit();
+      if (e.key === "Backspace") {
+        dispatch({ type: actionTypes.BACKSPACE });
+        return;
+      }
+      if (
+        state.guesses[state.currentGuess].length < 5 &&
+        e.key.match(/^[A-z]$/)
+      ) {
+        state.guesses[state.currentGuess] += e.key.toLowerCase();
+        setData({ ...state });
+        // console.log(memoState.current);
+        // dispatch new state changes here
+      }
+    };
+    dispatch({ type: actionTypes.SET_GAME, action: "set" });
+    window.addEventListener("keyup", handleKeyUp);
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [
+    dispatch,
+    // memoState,
+    state?.word,
+    state?.guesses,
+    state?.isGuessed,
+    state.currentGuess,
+    state?.loser,
+    state?.winner,
+    state?.words,
+  ]);
+  const handleReset = () =>
+    dispatch({ type: actionTypes.SET_GAME, action: "reset" });
 
-  useEffect(() => {
-    setData(notes);
-  }, [notes]);
-
-  return { data, setData, handleSubmit, handleDelete };
+  return { data, handleReset };
 };
