d843400eaf7076a5a6649690cd9ccc66aa5c16b7
dreywesson <dreywesson@gmail.com>
2022-10-27 10:50:19 +0100

    double re-rendering solved

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,22 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import { Item } from "./Item";
 
 function App() {
-    const [empty, setEmpty] = useState(false);
     return (
         <div className="App">
-            <h1>Shopping Cart</h1>
-            <p>
-                <span>Number of items: </span>
-                <span>6</span>
-            </p>
-            <p>
-                <span>Total: </span>{" "}
-                <span>{JSON.parse(localStorage.getItem("total"))}</span>
-            </p>
-            <button onClick={() => setEmpty(true)}>Clear shopping cart</button>
-            <Item empty={empty} />
+            <Item />
         </div>
     );
 }
--- shopping-cart/src/Item.js
+++ shopping-cart/src/Item.js
@@ -1,27 +1,83 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 const dataList = [
     { name: "Egg", quantity: 1, price: 2.99 },
     { name: "Milk", quantity: 2, price: 3.98 },
     { name: "Cheese", quantity: 3, price: 3.99 },
 ];
-export const Item = ({ empty }) => {
-    const total = useRef(0);
+export const Item = () => {
     const [edit, setEdit] = useState(false);
     const [list, setList] = useState(dataList);
+    const [sum, setSum] = useState(0);
+    const [empty, setEmpty] = useState(false);
+    const [tmp, setTmp] = useState(dataList);
+
+    const handleLogic = useCallback((price, quantity, i) => {
+        const itemTotal = price * quantity;
+        let val = i === 0 ? 0 : JSON.parse(localStorage.getItem("total"));
+        val += itemTotal;
+        localStorage.setItem("total", JSON.stringify(val));
+        return itemTotal;
+    }, []);
 
     // TODO
-    // if quantity === 0 || empty remove item
     useEffect(() => {
-        localStorage.setItem("total", JSON.stringify(total.current));
+        setSum(() => JSON.parse(localStorage.getItem("total")));
+        // localStorage.setItem("tmp", list);
+
         if (empty) setList(() => []);
     }, [empty]);
 
+    const removeItemHandler = (i) =>
+        setList((current) => {
+            const update = structuredClone(current);
+            const newTotal = +(
+                JSON.parse(localStorage.getItem("total")) -
+                update[i].quantity * update[i].price
+            ).toFixed(2);
+            setSum(() => newTotal);
+            localStorage.setItem("total", JSON.stringify(newTotal));
+            for (let j = i; j < update.length; j++) {
+                update[j] = update[j + 1];
+            }
+            update.length = update.length - 1;
+            return update;
+        });
+    const handleMinusAdd = (type, i, name, newVal) => {
+        setList((current) => {
+            const update = structuredClone(current);
+            type === "minus"
+                ? (update[i].quantity -= 1)
+                : (update[i].quantity += 1);
+            return update;
+        });
+    };
+
+    const tmpHandler = (name, newSum) =>
+        setTmp((prev) => ({
+            ...prev,
+            [name]: newSum,
+        }));
+    const handleSafe = (dataList) => {
+        localStorage.setItem("tmp", dataList);
+    };
+    console.log(tmp);
     return (
         <div>
+            <div>
+                <h1>Shopping Cart</h1>
+                <p>
+                    <span>Number of items: </span>
+                    <span>6</span>
+                </p>
+                <p>
+                    <span>Total: </span> <span>{sum.toFixed(2)}</span>
+                </p>
+                <button onClick={() => setEmpty(true)}>
+                    Clear shopping cart
+                </button>
+            </div>
             {list?.map(({ price, quantity, name }, i) => {
-                const itemTotal = price * quantity;
-                total.current += itemTotal;
-                localStorage.setItem("total", JSON.stringify(total.current));
+                const itemTotal = handleLogic(price, quantity, i);
 
                 return (
                     <div key={i + 1}>
@@ -41,40 +97,7 @@ export const Item = ({ empty }) => {
                                 <button onClick={() => setEdit(!edit)}>
                                     Change quantity
                                 </button>
-                                <button
-                                    onClick={() =>
-                                        setList((current) => {
-                                            const update =
-                                                structuredClone(current);
-                                            const newTotal =
-                                                itemTotal -
-                                                update[i].quantity *
-                                                    update[i].price;
-                                            // console.log(
-                                            //     itemTotal,
-                                            //     newTotal,
-                                            //     update[i].quantity,
-                                            //     update[i].price
-                                            // );
-                                            for (
-                                                let j = i;
-                                                j < update.length;
-                                                j++
-                                            ) {
-                                                update[j] = update[j + 1];
-                                            }
-                                            update.length = update.length - 1;
-
-                                            total.current = newTotal;
-
-                                            localStorage.setItem(
-                                                "total",
-                                                JSON.stringify(newTotal)
-                                            );
-                                            return update;
-                                        })
-                                    }
-                                >
+                                <button onClick={() => removeItemHandler(i)}>
                                     Remove
                                 </button>
                             </div>
@@ -87,51 +110,45 @@ export const Item = ({ empty }) => {
                                 }}
                             >
                                 <input
-                                    type="text"
+                                    type="number"
                                     name={name}
                                     value={list[i].quantity}
                                     onChange={(e) => {
                                         e.preventDefault();
-                                        const newVal = e.target.value;
+                                        const newVal = +e.target.value;
+                                        const newSum = newVal * list[i].price;
                                         setList((current) => {
                                             const update =
                                                 structuredClone(current);
                                             update[i].quantity = newVal;
                                             return update;
                                         });
+                                        tmpHandler(name, newSum);
                                     }}
                                 />
                                 <div>
                                     <button
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            setList((current) => {
-                                                const update =
-                                                    structuredClone(current);
-                                                update[i].quantity += 1;
-                                                return update;
-                                            });
-                                        }}
+                                        onClick={() => handleMinusAdd("add", i)}
                                     >
                                         Add
                                     </button>
                                     <button
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            setList((current) => {
-                                                const update =
-                                                    structuredClone(current);
-                                                update[i].quantity -= 1;
-                                                return update;
-                                            });
-                                        }}
+                                        onClick={() =>
+                                            handleMinusAdd("minus", i)
+                                        }
                                     >
                                         Remove
                                     </button>
                                 </div>
                                 <div>
-                                    <button>Save</button>
-                                    <button>Cancel</button>
+                                    <button onClick={() => {}}>Save</button>
+                                    <button
+                                        onClick={() => {
+                                            setEdit(!edit);
+                                        }}
+                                    >
+                                        Cancel
+                                    </button>
                                 </div>
                             </div>
                         )}
